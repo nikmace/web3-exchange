269f2fb9dbbf04d3317c29b2579a2677df0093b3
Nikita Baranov <nikmacentosh@gmail.com>
2022-01-08 16:14:12 +0200

    Solidity Contract

---
+++ smart-contract/contracts/Transactions.sol
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+contract Transactions {
+    uint256 transactionCounter;
+
+    event Transfer(
+        address from,
+        address receiver,
+        uint256 amount,
+        string message,
+        uint256 timestamp,
+        string keyword
+    );
+
+    struct TransferStruct {
+        address sender;
+        address receiver;
+        uint amount;
+        string message;
+        uint256 timestamp;
+        string keyword;
+    }
+
+    TransferStruct[] transactions;
+
+    function addToBlockchain() public {}
+
+    function getAllTransactions() public view returns (TransferStruct[] memory) {
+
+        //return transactions;
+    }
+
+    function getTransactionCount() public view returns (uint256) {
+        
+        //return transactions.count;
+    }
+}
